242e00f18202c62efa1e732193ec2f99e5321263
arjunkdot <81557653+ArjunCybro@users.noreply.github.com>
2024-07-07 13:13:49 +0530

    feat: logging out

--- app/components/navigation/header.tsx
+++ app/components/navigation/header.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { MdOutlineNotifications } from "react-icons/md";
 import Notification from "../notification/Notification";
-
+import { supabase } from "@/lib/supabaseClient";
+import { useRouter } from "next/navigation";
 function Header() {
+  const router = useRouter();
+  const [loggedInUser, setLoggedInUser] = useState<{} | null>(null);
+  useEffect(() => {
+    async function checkIfLoggedIn() {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setLoggedInUser(user);
+    }
+    checkIfLoggedIn();
+  }, [loggedInUser]);
+
+  const handleLogout = async (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    const { error } = await supabase.auth.signOut();
+    if (!error) {
+      router.push("/");
+    }
+  };
   return (
     <div className="navbar bg-base-100 p-0 border border-b border-slate-200">
       <div className="container">
@@ -30,37 +52,39 @@ function Header() {
               <Notification />
             </div>
           </div>
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar">
-              <div className="w-8 rounded-full">
-                <Image
-                  alt="Tailwind CSS Navbar component"
-                  src="/avatar.jpg"
-                  width={36}
-                  height={36}
-                />
+          {loggedInUser ? (
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar">
+                <div className="w-8 rounded-full">
+                  <Image
+                    alt="Tailwind CSS Navbar component"
+                    src="/avatar.jpg"
+                    width={36}
+                    height={36}
+                  />
+                </div>
               </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a onClick={(e) => handleLogout(e)}>Logout</a>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
+          ) : null}
         </div>
       </div>
     </div>
